387226807e94d38999a7e302c68da8ad237e340e
Shashank <shashank@Shashanks-MacBook-Air.local>
2019-03-07 03:34:40 +0530

    set the correct timer

--- v3/atrang.js
+++ v3/atrang.js
@@ -7,7 +7,7 @@ var currentDate=new Date();
 function showTime(){
 	setTimeout(function(){
 		var currentDate=new Date();
-    	$(".date-days").text(6-currentDate.getDate());
+    	$(".date-days").text(35-currentDate.getDate());
     	$(".date-hrs").text(23-currentDate.getHours());
     	$(".date-mins").text(60-currentDate.getMinutes());
     	$(".date-secs").text(60-currentDate.getSeconds());
