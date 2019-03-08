c4fb0285ad6a87810975cd42e8c36d8a1476b545
Shashank <shashank@Shashanks-MacBook-Air.local>
2019-03-08 22:27:45 +0530

    added events and speakers ers kers

--- v3/atrang.js
+++ v3/atrang.js
@@ -15,5 +15,37 @@ function showTime(){
     			
 	}, 1000);
 }
+$(function(){
+  $(".animeq").tooltip({
+  	title:"Take the anime quiz to test your knowledge about the japanese art and win prizes along the way",
+  	placement:'top',
+  	animation: true,
+  	delay: 100
+  });
+   $(".how").tooltip({
+  	title:"Tell us what you think about the ending of a movie or a book and about how you feel it should've ended. The best one wins the prize",
+  	placement:'top',
+  	animation: true,
+  	delay: 100
+  });
+   $(".writerblog").tooltip({
+  	title:"Put your writing skills on the line and write about a topic in your own words...",
+  	placement:'top',
+  	animation: true,
+  	delay: 100
+  });
+   $(".putonashow").tooltip({
+  	title:"offline event.... put on a show.... you know what we mean",
+  	placement:'top',
+  	animation: true,
+  	delay: 100
+  });
+   $(".charclick").tooltip({
+  	title:"Take a quiz and pick out the correct character from the given images....",
+  	placement:'top',
+  	animation: true,
+  	delay: 100
+  });
+});
 
 
